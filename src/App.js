c95b18dda0f16bb59f1d9d2ddbb62a68d8a19cf3
avfrontend <avraam.kastrappis@gmail.com>
2021-07-22 00:22:19 +0300

    react hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import ReactDOM from "react-dom";
 import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese"></Pet>
-      <Pet name="Popper" animal="Bird" breed="Cockatiel"></Pet>
-      <Pet name="Sudo" animal="Dog" breed="Weaten Terrier"></Pet>
+      <SearchParams />
     </div>
   );
 };

1b115b29fc2425960aba039ad3c1e575d90cd612
avfrontend <avraam.kastrappis@gmail.com>
2021-07-22 14:30:53 +0300

    useEffect hook

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,12 +1,29 @@
-import { useState } from "react";
+import { async } from "q";
+import { useState, useEffect } from "react";
+import Pet from "./Pet";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
-  const [location, setLocation] = useState("Seattle, WA");
+  const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
-  const [breed, setbreed] = useState("");
+  const [breed, setBreed] = useState("");
+  const [pets, setPets] = useState([]);
   const breeds = [];
+
+  useEffect(() => {
+    requestPets();
+  }, []);
+
+  async function requestPets() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+
+    const json = await res.json();
+    console.log(json);
+    setPets(json.pets);
+  }
 
   return (
     <div className="search-params">
@@ -40,7 +57,7 @@ const SearchParams = () => {
           Breed
           <select
             id="breed"
-            value={animal}
+            value={breed}
             onChange={(e) => setBreed(e.target.value)}
             onBlur={(e) => setBreed(e.target.value)}
           >
@@ -54,6 +71,14 @@ const SearchParams = () => {
         </label>
         <button>Submit</button>
       </form>
+      {pets.map((pet) => (
+        <Pet
+          name={pet.name}
+          animal={pet.animal}
+          breed={pet.breed}
+          key={pet.id}
+        />
+      ))}
     </div>
   );
 };

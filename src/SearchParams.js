e0255c8460c9ae5cd2a40c2fa6d3b0169715fbbd
avfrontend <avraam.kastrappis@gmail.com>
2021-07-24 22:01:19 +0300

    component composition

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -77,14 +77,7 @@ const SearchParams = () => {
         </label>
         <button>Submit</button>
       </form>
-      {pets.map((pet) => (
-        <Pet
-          name={pet.name}
-          animal={pet.animal}
-          breed={pet.breed}
-          key={pet.id}
-        />
-      ))}
+      <Results pets={pets} />
     </div>
   );
 };
